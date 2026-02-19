f484d83eb30fb7e037763c5534d99636f843b897
Eduardo Bussien <edu.buma@gmail.com>
2026-02-18 20:08:13 -0700

    Initial setup: imports and Great Pyramid location

---
+++ src/alignment.py
@@ -0,0 +1,18 @@
+"""
+Archaeo-Astronomy Alignment Tool. By EduardoBussien. 02/17/2026
+
+"""
+
+from astropy.coordinates import EarthLocation, AltAz, get_sun, SkyCoord
+from astropy.time import Time
+import astropy.units as u
+from datetime import datetime
+import numpy as np
+
+# Great Pyramid of Giza
+PYRAMID_LAT = 29.9792 * u.deg  # degrees North
+PYRAMID_LON = 31.1342 * u.deg  # degrees East
+PYRAMID_HEIGHT = 0 * u.m       # meters above sea level 
+
+# EarthLocation object
+pyramid_location = EarthLocation(lat=PYRAMID_LAT, lon=PYRAMID_LON, height=PYRAMID_HEIGHT)
